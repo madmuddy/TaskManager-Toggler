b354c771addc6701582c377c9d462a91202da22d
madmuddy <mmamma496@gmail.com>
2021-10-29 19:16:48 +0500

    Initial commit

---
+++ TaskManager/Core.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public static class Core
+    {
+        public static int EnableTaskManager()
+        {
+            try
+            {
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static int DisaableTaskManager()
+        {
+            try
+            {
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static int ToggleTaskManager()
+        {
+            try
+            {
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
